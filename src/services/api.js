dff1858c5525349df29ae9c8a670edfe825e8113
Gabriel Perini <gabrielprestesperini@hotmail.com>
2019-12-18 16:58:17 -0800

    Add API base

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,52 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'https://api.github.com',
-});
 
-export default api;
+class Api {
+
+    Axios = axios.create({
+        baseURL: '',
+    });
+
+    getCardapio(){
+        var cardapio;
+        this.Axios.get('api/cardapio')
+        .then(function(response){
+            cardapio = JSON.parse(response);
+        });
+        return cardapio;
+    }
+
+    getPromocoes(){
+        var promocoes;
+        this.Axios.get('api/promocoes')
+        .then(function(response){
+            promocoes = JSON.parse(response);
+        });
+        return promocoes;
+    }
+
+    getDetails(id){
+        var details;
+        this.Axios.get('api/details/'+id)
+        .then(function(response){
+            details = JSON.parse(response);
+        });
+        return details;
+    }
+
+    getStatusPedido(id){
+        var status;
+        this.Axios.get('api/status/'+id)
+        .then(function(response){
+            status = JSON.parse(response);
+        });
+        return status;
+    }
+
+}
+
+export default Api = new Api;
+
+
+
+
